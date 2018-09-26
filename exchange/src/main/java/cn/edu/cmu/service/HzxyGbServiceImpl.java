28abb59a116530be5b31d5116c030857b2e99400
jshand <zhangjinshan21f@163.com>
2018-09-26 16:52:04 +0800

    添加菜单，新生成合作协议的 实体类

--- exchange/src/main/java/cn/edu/cmu/service/HzxyGbServiceImpl.java
+++ exchange/src/main/java/cn/edu/cmu/service/HzxyGbServiceImpl.java
@@ -4,7 +4,6 @@ import cn.edu.cmu.dao.HzxyGbMapper;
 import cn.edu.cmu.domain.HzxyGb;
 import cn.edu.cmu.domain.HzxyGbParams;
 import cn.edu.cmu.framework.web.BaseService;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,10 +17,10 @@ public class HzxyGbServiceImpl extends BaseService<HzxyGb, HzxyGbParams, HzxyGbM
     public List list(HzxyGb HzxyGb) {
         HzxyGbParams ex = new HzxyGbParams();
         if(HzxyGb != null){
-            HzxyGbParams.Criteria c = ex.createCriteria();
-            if(StringUtils.isNotEmpty(HzxyGb.getGbmc())){
-                c.andGbmcLike("%"+HzxyGb.getGbmc()+"%");
-            }
+//            HzxyGbParams.Criteria c = ex.createCriteria();
+//            if(StringUtils.isNotEmpty(HzxyGb.getGbmc())){
+//                c.andGbmcLike("%"+HzxyGb.getGbmc()+"%");
+//            }
         }
         return dao.selectByExample(ex);
     }
