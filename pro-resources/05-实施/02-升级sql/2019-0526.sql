0f7bf08f339877a7615184f1da5bf0a257ea3fe8
jshand <zhangjinshan21f@163.com>
2019-05-26 10:15:06 +0800

    会议申报中会议背景及必要性中会议中涉及的所有组织无涉台问题 填写会议计划的会议类型加上港，澳，台间会议，论坛报告，讲座

---
+++ pro-resources/05-实施/02-升级sql/2019-0526.sql
@@ -0,0 +1,11 @@
+
+
+
+insert into t_dm_hylx (CODE, NAME, VALID, CREATE_TIME)
+values ('01', '国际会议', '1', to_date('05-10-2018 20:27:11', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into t_dm_hylx (CODE, NAME, VALID, CREATE_TIME)
+values ('02', '两国间报告、论坛、讲座会议', '1', to_date('05-10-2018 20:27:11', 'dd-mm-yyyy hh24:mi:ss'));
+
+insert into t_dm_hylx (CODE, NAME, VALID, CREATE_TIME)
+values ('03', '港，澳，台间会议，论坛报告，讲座', '1', to_date('26-05-2019 09:42:28', 'dd-mm-yyyy hh24:mi:ss'));
