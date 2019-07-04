ab50f98147e4145984614c802867ef16324593d0
jshand <zhangjinshan21f@163.com>
2019-07-04 09:09:26 +0800

    更新近期执行的sql 数据结构

--- pro-resources/05-实施/02-升级sql/2019-0627.sql
+++ pro-resources/05-实施/02-升级sql/2019-0627.sql
@@ -2,3 +2,5 @@
 alter table T_WBJD_SQ add operator VARCHAR2(500);
 -- Add comments to the columns
 comment on column T_WBJD_SQ.operator is '申请操作人id';
+
+---------------已执行 2019年7月1日09:06:04------------------------------
